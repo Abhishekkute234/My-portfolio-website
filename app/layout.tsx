21bf8e4bd1d487a11e22c5017374b037e2508ec9
Abhishekkute234 <kuteabhishek2004@gmail.com>
2024-12-21 09:45:26 +0530

    minor change

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,65 +1,90 @@
-import "@/styles/globals.css";
-import { Metadata, Viewport } from "next";
-import { Link } from "@nextui-org/link";
-import clsx from "clsx";
+import React from 'react';
+import type { Metadata } from 'next';
+import { Inter, Silkscreen } from 'next/font/google';
 
-import { Providers } from "./providers";
+import './globals.css';
+import NavBar from '@/components/layout/NavBar';
+import Footer from '@/components/layout/Footer';
 
-import { siteConfig } from "@/config/site";
-import { fontSans } from "@/config/fonts";
-import { Navbar } from "@/components/navbar";
+const inter = Inter({
+  subsets: ['latin'],
+  weight: ['100', '200', '300', '400', '500', '600', '700', '800', '900'],
+  variable: '--font-inter',
+});
+
+const silkscreen = Silkscreen({
+  subsets: ['latin'],
+  weight: ['400', '700'],
+  variable: '--font-silkscreen',
+});
 
 export const metadata: Metadata = {
-  title: {
-    default: siteConfig.name,
-    template: `%s - ${siteConfig.name}`,
-  },
-  description: siteConfig.description,
-  icons: {
-    icon: "/favicon.ico",
-  },
-};
-
-export const viewport: Viewport = {
-  themeColor: [
-    { media: "(prefers-color-scheme: light)", color: "white" },
-    { media: "(prefers-color-scheme: dark)", color: "black" },
+  title: 'Adel Guitoun',
+  description: `Hi 👋, I'm Adel Guitoun. I'm a passionate software engineer with a knack for creating scalable, high-performance applications and reusable systems. With a strong foundation in front-end and back-end technologies, I specialize in crafting solutions that are as impactful for users as they are efficient for developers.`,
+  creator: 'Adel Guitoun',
+  applicationName: 'My website',
+  keywords: [
+    'Adel Guitoun',
+    'Adel',
+    'Guitoun',
+    'Portfolio',
+    'Next.js',
+    'Software engineer',
   ],
+  icons: {
+    icon: './favicon.ico',
+    apple: '/apple-touch-icon.png',
+    other: [
+      {
+        rel: 'icon',
+        url: '/favicon-32x32.png',
+        sizes: '32x32',
+      },
+      {
+        rel: 'icon',
+        url: '/favicon-16x16.png',
+        sizes: '16x16',
+      },
+    ],
+  },
+  openGraph: {
+    type: 'website',
+    url: 'https://www.adelguitoun.com',
+    title: 'Adel Guitoun - Software Engineer',
+    description: `Hi 👋, I'm Adel Guitoun. I'm a passionate software engineer with a knack for creating scalable, high-performance applications and reusable systems. With a strong foundation in front-end and back-end technologies, I specialize in crafting solutions that are as impactful for users as they are efficient for developers.`,
+    images: [
+      {
+        url: '/opengraph-image.jpg',
+        width: 1200,
+        height: 628,
+        alt: 'Adel Guitoun',
+      },
+    ],
+  },
+  twitter: {
+    title: 'Adel Guitoun - Software Engineer',
+    description: `Hi 👋, I'm Adel Guitoun. I'm a passionate software engineer with a knack for creating scalable, high-performance applications and reusable systems. With a strong foundation in front-end and back-end technologies, I specialize in crafting solutions that are as impactful for users as they are efficient for developers.`,
+    card: 'summary_large_image',
+    site: '@AdelGuitoun',
+    images: '/opengraph-image.jpg',
+  },
+  verification: {
+    google: 'k83JtkKiU8MvRqLS05NLwZPSOGLJXbZpzln4wdJ-iPI',
+  },
 };
 
 export default function RootLayout({
   children,
-}: {
+}: Readonly<{
   children: React.ReactNode;
-}) {
+}>) {
   return (
-    <html suppressHydrationWarning lang="en">
-      <head />
-      <body
-        className={clsx(
-          "min-h-screen bg-background font-sans antialiased",
-          fontSans.variable,
-        )}
-      >
-        <Providers themeProps={{ attribute: "class", defaultTheme: "dark" }}>
-          <div className="relative flex flex-col h-screen">
-            <Navbar />
-            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
-              {children}
-            </main>
-            <footer className="w-full flex items-center justify-center py-3">
-              <Link
-                isExternal
-                className="flex items-center gap-1 text-current"
-                href="https://easyui.pro"
-                title="easyui.pro homepage"
-              >
-                <span className="text-default-600">Powered by</span>
-                <p className="text-primary">Easy UI</p>
-              </Link>
-            </footer>
-          </div>
-        </Providers>
+    <html lang="en">
+      <body className={`${inter.variable} ${silkscreen.variable} relative`}>
+        <NavBar />
+        <main>{children}</main>
+        <Footer />
+        <div className="main-mask pointer-events-none absolute inset-0 -z-50" />
       </body>
     </html>
   );
--- app/page.tsx
+++ app/page.tsx
@@ -1,67 +1,21 @@
-import { Link } from "@nextui-org/link";
-import { Snippet } from "@nextui-org/snippet";
-import { Code } from "@nextui-org/code";
-import { button as buttonStyles } from "@nextui-org/theme";
-
-import { siteConfig } from "@/config/site";
-import { title, subtitle } from "@/components/primitives";
-import { GithubIcon } from "@/components/icons";
-import Badge from "@/components/chip";
-import TrustedBy from "@/components/trusted";
-import PrototypeImg from "@/components/prototype-img";
-import FeaturesBento from "@/components/feature-bento";
-import LightBg from "@/components/light-bg";
-import Integrations from "@/components/integrations";
-import { LastButNotLeast } from "@/components/last-but-not-least";
+import About from '@/components/about/About';
+import Articles from '@/components/articles/Articles';
+import Contact from '@/components/contact/Contact';
+import Hero from '@/components/hero/Hero';
+import ProjectsSection from '@/components/projects/ProjectsSection';
+import Reviews from '@/components/review/Reviews';
+import Techs from '@/components/techs/Techs';
 
 export default function Home() {
   return (
-    <section className="flex flex-col items-center justify-center gap-4 py-8 md:py-10 ">
-      <Badge />
-      <div className="inline-block max-w-sm lg:max-w-4xl text-center justify-center text-2xl">
-        <h1 className={title({ size: "lg" })}>Make&nbsp;</h1>
-        <h1 className={title({ color: "violet", size: "lg" })}>
-          Beautiful&nbsp;
-        </h1>
-        <br />
-        <h1 className={title({ size: "lg" })}>Websites using Easy NextUI</h1>
-        <h2 className="font-normal text-gray-500 py-2">
-          Beautiful, fast and modern Easy UI template.
-        </h2>
-      </div>
-
-      <div className="flex gap-3">
-        <Link
-          isExternal
-          className={buttonStyles({
-            color: "primary",
-            radius: "full",
-            variant: "shadow",
-          })}
-          href={siteConfig.links.docs}
-        >
-          Documentation
-        </Link>
-        <Link
-          isExternal
-          className={buttonStyles({ variant: "bordered", radius: "full" })}
-          href={siteConfig.links.github}
-        >
-          <GithubIcon size={20} />
-          GitHub
-        </Link>
-      </div>
-      <TrustedBy />
-      <PrototypeImg />
-      <div className="mt-20 lg:mt-60 flex-col justify-center items-center mx-auto">
-        <h1 className="text-2xl lg:text-4xl font-semibold flex- justify-center items-center mx-auto text-center">
-          Features
-        </h1>
-        <FeaturesBento />
-      </div>
-      <LightBg />
-      <Integrations />
-      <LastButNotLeast />
-    </section>
+    <>
+      <Hero />
+      <About />
+      <Techs />
+      <ProjectsSection />
+      <Articles />
+      <Reviews />
+      <Contact />
+    </>
   );
 }
