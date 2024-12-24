93ff2f71ac153c44c5bfe826562e3700408fea82
Abhishekkute234 <kuteabhishek2004@gmail.com>
2024-12-24 14:33:06 +0530

    minor

--- components/layout/NavLinks.tsx
+++ components/layout/NavLinks.tsx
@@ -4,11 +4,15 @@ const NavLinks = () => {
   return (
     <nav className="mt-16 flex flex-col gap-8 font-Silkscreen text-3xl md:mt-0 md:flex-row md:text-sm">
       <Link className="py-4 md:py-0 md:hover:opacity-70" href="/">
-        Projects
+        Experiences
       </Link>
 
       <Link className="py-4 md:py-0 md:hover:opacity-70" href="/">
-        Blog
+        Skills
+      </Link>
+
+      <Link className="py-4 md:py-0 md:hover:opacity-70" href="/">
+        Projects
       </Link>
     </nav>
   );
