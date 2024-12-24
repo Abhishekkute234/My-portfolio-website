00da76ac157bcd6921aea2cd33220cd6ce635b94
Abhishekkute234 <kuteabhishek2004@gmail.com>
2024-12-24 14:35:32 +0530

    minor change

--- components/layout/NavLinks.tsx
+++ components/layout/NavLinks.tsx
@@ -14,6 +14,9 @@ const NavLinks = () => {
       <Link className="py-4 md:py-0 md:hover:opacity-70" href="/">
         Projects
       </Link>
+      <Link className="py-4 md:py-0 md:hover:opacity-70" href="/">
+        Notes
+      </Link>
     </nav>
   );
 };
