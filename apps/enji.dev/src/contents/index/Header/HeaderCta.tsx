92726638006cbf8bd30bd33397b7b96b253c43c3
Abhishek Kute <148034480+Abhishekkute234@users.noreply.github.com>
2025-07-15 07:29:08 +0530

    Update HeaderCta.tsx

--- apps/enji.dev/src/contents/index/Header/HeaderCta.tsx
+++ apps/enji.dev/src/contents/index/Header/HeaderCta.tsx
@@ -36,7 +36,7 @@ function ButtonResume() {
     <a
       target="_blank"
       rel="noreferrer nofollow"
-      href="https://drive.google.com/file/d/1xnILUBAI3PUBJbluLUVnK4-lBRt7dzVL/view"
+      href="https://drive.google.com/file/d/1_Op_qm2uggKuJT-S5uzfF8eG99Uuu4Ct/view"
       className={clsx('button button--ghost px-2', 'md:button--big md:px-2')}
     >
       <DocumentIcon className={clsx('h-5 w-5')} />
